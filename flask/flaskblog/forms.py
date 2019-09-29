d0b8049f4604144831e6a209cd4ac08d9cb1bc70
manasbichoo <manas16b@gmail.com>
2019-09-29 19:14:12 +0530

    modified on 29/09/2019

--- flask/flaskblog/forms.py
+++ flask/flaskblog/forms.py
@@ -2,70 +2,69 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email,  EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
 
-
 class RegistrationForm(FlaskForm):
-	username=StringField('Username', 
-	validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-	email = StringField('Email', validators=[DataRequired(), Email()])
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
-	password= PasswordField('Password', validators=[DataRequired()])
-
-	confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-
-	submit = SubmitField('Sign Up')
-
-	def validate_username(self, username):
-		user=User.query.filter_by(username=username.data).first()
-		if user:
-			raise ValidationError('This username is already taken. Please choose another one.')
-
-	def validate_email(self, email):
-		emailv=User.query.filter_by(email=email.data).first()
-		if emailv:
-			raise ValidationError('This Email is already taken. Please choose another one.')
-
-
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
-
-	email = StringField('Email', validators=[DataRequired(), Email()])
-
-	password= PasswordField('Password', validators=[DataRequired()])
-
-	remember= BooleanField('Remember Me')
-
-	submit = SubmitField('Log In')
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 
 class UpdateAccountForm(FlaskForm):
-	username=StringField('Username', 
-	validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
 
-	email = StringField('Email', validators=[DataRequired(), Email()])
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
 
-	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
 
-	submit = SubmitField('Update')
-
-	def validate_username(self, username):
-		if username.data != current_user.username:
-		   user=User.query.filter_by(username=username.data).first()
-		   if user:
-			   raise ValidationError('This username is already taken. Please choose another one.')
-
-	def validate_email(self, email):
-		if email.data != current_user.email:
-		   emailv=User.query.filter_by(email=email.data).first()
-		   if emailv:
-			   raise ValidationError('This Email is already taken. Please choose another one.')
 
 class PostForm(FlaskForm):
-	title = StringField('Title', validators=[DataRequired()])
-	content = TextAreaField ('Content', validators=[DataRequired()])
-	submit = SubmitField('Post')
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+##################################################################################
+class ProjectForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    url = StringField('URL', validators=[DataRequired()])
+    language = StringField('Language', validators=[DataRequired()])
+    author =  StringField('Author', validators=[DataRequired()])
+    submit = SubmitField('Add')
--- flask/flaskblog/routes.py
+++ flask/flaskblog/routes.py
@@ -6,6 +6,10 @@ from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import pandas as pd
+import urllib.request
+import sqlite3
+from bs4 import BeautifulSoup as bs
 
 
 @app.route("/")
@@ -142,3 +146,59 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+
+    ##################################################
+@app.route("/project", methods=['GET', 'POST'])
+@login_required
+def project():
+    conn = sqlite3.connect(r"E:\COLLEGE PROJECTS\collegeproject\example.db")
+    if request.method == 'POST': 
+        result = request.form["URL"]
+        
+        
+
+        df = {'Projects':[],'Link':[],'Language':[],'Name':[]}
+
+        inputurl=result
+        url=inputurl+'?tab=repositories'
+        content = urllib.request.urlopen(url).read()
+        soup = bs(content,'html.parser')
+
+        for tag in soup.find_all('a',attrs={'itemprop':'name codeRepository'}):
+            df['Projects'].append(str(tag.text))
+                
+        for link in soup.find_all('a',attrs={'itemprop':'name codeRepository'}):
+            df['Link'].append('https://github.com'+str(link.get('href')))
+        
+        nm=soup.find('span', attrs={'class':'p-name vcard-fullname d-block overflow-hidden'})
+        name=nm.text
+
+        for div in soup.find_all('div', attrs={'class':'col-10 col-lg-9 d-inline-block'}):
+            x=div.find('span',attrs={'itemprop':'programmingLanguage'})
+            if x is None:
+                df['Language'].append('None')
+                df['Name'].append(name)
+            else:
+                df['Language'].append(x.text)
+                df['Name'].append(name)
+       
+        df1=pd.DataFrame(df)
+        
+        #Database
+        cur = conn.cursor()
+        df1.to_sql('Projects', conn,if_exists='append', index=False)
+        df2=pd.read_sql('select * from Projects', conn) 
+        #pd.read_sql('select * from Projects', conn)
+        conn.commit()
+        conn.close()
+
+        return render_template('index.html',result=url, table = df2.to_html()) # renders template: index.html with argument result = polarity value calculated
+    else:
+        conn = sqlite3.connect(r"E:\COLLEGE PROJECTS\collegeproject\example.db")
+        cur = conn.cursor()
+        df3=pd.read_sql('select * from Projects', conn) 
+        conn.commit()
+        conn.close()
+        return render_template('index.html', table = df3.to_html())    
